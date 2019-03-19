dd5f25dfda796c503e4c31baeec9030f2ae7da71
Philippe HAURET <venteuil.oreste@gmail.com>
2019-03-19 20:21:55 +0100

    fini maquette articles

--- views/accueil.php
+++ views/accueil.php
@@ -6,8 +6,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?php echo $title; ?> </title>
+    <title>SportSearch</title>
 
+    <link rel="icon" href="../public/img/logo.ico">
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" type="text/css" href="../public/bootstrap/css/bootstrap.min.css" />
 
@@ -20,51 +21,57 @@
 
 </head>
 <body>
-    <div id="wrapper">
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <!-- Img -->
-                    <a href="#">
-                        Logo
-                    </a>
-                </li>
-                <br />
-                <li>
-                  <!--<img src="">--><a href="#">Tous les sports</a>
-                </li>
-                <li>
-                  <!--<img src="">--><a href="#">Football</a>
-
-                </li>
-                <li>
-                    <!--<img src="">--><a href="#">Rugby</a>
-                </li>
-                <li>
-                    <!--<img src="">--><a href="#">Basket</a>
-                </li>
-            </ul>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 side">
+                <nav class="navbar">
+                    <a class="navbar-brand logo" href="#"><img width="30" height="30" src="../public/img/logo.svg"><strong>Sport</strong>Search</a>
+                    <form class="navbar-form navbar-right inline-form">
+                        <div class="form-group">
+                            <input type="search" class="input-sm form-control" placeholder="Recherche">
+                        </div>
+                    </form>
+                    <ul class="nav navbar-nav menu">
+                        <li><a href="#"><i class="icon"><img src="../public/img/all.png" class=""></i>Tous les sports</a></li>
+                        <li><a href="#"><i class="icon"><img src="../public/img/foot.png" class=""></i>Football</a></li>
+                        <li><a href="#"><i class="icon"><img src="../public/img/rugby.png" class=""></i>Rugby</a></li>
+                        <li><a href="#"><i class="icon"><img src="../public/img/basket.png" class=""></i>Basket</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-10 main" style="display: none;">
+                <div>
+                    Bulles
+                    <!-- TODO : BULLES D3J -->
+                </div>
+            </div>
+            <div class="col-lg-10 main">
+                <div class="ensemble">
+                    <div class="article col-lg-10">
+                        <img src="../public/img/didi.png" class="imgArticle">
+                        <h4>PSG - OM (3-1) : Didier Deschamps réagit sur le comportement de Kylian Mbappé</h4>
+                        <h5 class="content">But! Football Club - Il y a 3 heures 18/03/2019</h5>
+                        <h5 class="ngrams">Mbappé : 20  Neymar : 18  Ligue 1 : 15  Didier Deschamps : 14</h5>
+                    </div>
+                    <div class="article col-lg-10">
+                        <img src="../public/img/didi.png" class="imgArticle">
+                        <h4>PSG - OM (3-1) : Didier Deschamps réagit sur le comportement de Kylian Mbappé</h4>
+                        <h5 class="content">But! Football Club - Il y a 3 heures 18/03/2019</h5>
+                        <h5 class="ngrams">Mbappé : 20  Neymar : 18  Ligue 1 : 15  Didier Deschamps : 14</h5>
+                    </div>
+                    <div class="article col-lg-10">
+                        <img src="../public/img/didi.png" class="imgArticle">
+                        <h4>PSG - OM (3-1) : Didier Deschamps réagit sur le comportement de Kylian Mbappé</h4>
+                        <h5 class="content">But! Football Club - Il y a 3 heures 18/03/2019</h5>
+                        <h5 class="ngrams">Mbappé : 20  Neymar : 18  Ligue 1 : 15  Didier Deschamps : 14</h5>
+                    </div>
+                </div>
+                
+            </div>
         </div>
-        
-        <!-- /#sidebar-wrapper -->
+        <!-- /#wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
-    
-    <div >
-        Bulles
-        <!-- TODO : BULLES D3J -->
-    </div>
-    <div style="display: none;">
-        <ul class="sidebar-nav">
-            <li>
-                <p>Article n°1</p><h3>Title</h3><img>
-            </li>
-            <li>
-                <p>Article n°2</p><h3>Title</h3><img>
-            </li>
-        </ul>
+        
     </div>
 
 
